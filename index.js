7bc586bcf41ce0d1815df8005e9b141d9b6d6757
edusousa78 <sousa.ej@gmail.com>
2024-08-09 10:08:49 -0300

    comitando a criação da index

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+// Criando a Função para Calcular o nével
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+// Adiciona valores às variaveis
+let vitorias = 60;
+let derrotas = 35;
+
+let resultado = calcularNivel(vitorias, derrotas);
+console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
